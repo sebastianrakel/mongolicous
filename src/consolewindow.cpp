061f5b2377d8a25b9089f3dbd3c62c83e7410862
Sebastian Rakel <sebastian@devunit.eu>
2016-01-18 14:25:42 +0100

    Test include for mongoclient

--- src/consolewindow.cpp
+++ src/consolewindow.cpp
@@ -1,6 +1,8 @@
 #include "lib/consolewindow.h"
 #include "ui_consolewindow.h"
 
+#include "mongo/client/dbclient.h"
+
 ConsoleWindow::ConsoleWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ConsoleWindow)
